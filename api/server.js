0ac11425e889ab78f1989cdffa273a23baece223
ennas-de <dev.abdulhakeem@gmail.com>
2023-07-22 02:56:09 +0100

    Add Updates

--- api/server.js
+++ api/server.js
@@ -1,28 +1,33 @@
 import express from "express";
-import api from './routes/index.js'
-import dotenv from 'dotenv'
+import api from "./routes/index.js";
+import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 
-dotenv.config()
-mongoose.connect(process.env.MONGODB_PATH, () => {
-    console.log('connect');
-}, (e) => console.log(e))
+dotenv.config();
+mongoose.connect(
+  process.env.MONGODB_PATH,
+  () => {
+    console.log("connect");
+  },
+  (e) => console.log(e)
+);
 
+const PORT = process.env.PORT || 9000;
+const origin = process.env.CORS_ORIGIN || "http://localhost:3000";
 
-const PORT = process.env.SERVER_PORT || 9000
-const origin = process.env.CORS_ORIGIN || 'http://localhost:3000'
+const app = express();
 
-const app = express()
+app.use(
+  cors({
+    origin,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded());
 
-app.use(cors({
-    origin
-}));
-app.use(express.json())
-app.use(express.urlencoded())
-
-app.use(api)
+app.use(api);
 
 app.listen(PORT, () => {
-    console.log(`Your app is running in http://localhost:${PORT}`)
-})
+  console.log(`Your app is running in http://localhost:${PORT}`);
+});
